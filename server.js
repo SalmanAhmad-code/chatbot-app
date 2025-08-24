21d6ef0d0e299c82c5aafeb1bcaddbc511cec7b6
Salman Ahmad <salmanahmadyt@gmail.com>
2025-08-24 20:38:23 +0500

    render

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import { OpenAI } from "openai";
 import fetch from "node-fetch";
 import path from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
